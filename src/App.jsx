11f7849238cd67f5b915d33726f360ef5ff363cc
AyloSrd <aylo.srd@gmail.com>
2020-10-13 15:16:53 +0200

    add react without jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,8 @@ function App() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>HTML Document</title>
+        <script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
+        <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
         <style>
           ${css}
         </style>
